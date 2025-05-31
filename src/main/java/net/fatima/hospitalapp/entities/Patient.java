0d57f2c32dabd13fec45d515b31a900178bc9582
FATIMA AIT LAMINE <fatima.aitlamine03@gmail.com>
2025-05-31 11:22:46 +0100

    final version without security

--- src/main/java/net/fatima/hospitalapp/entities/Patient.java
+++ src/main/java/net/fatima/hospitalapp/entities/Patient.java
@@ -1,7 +1,12 @@
 package net.fatima.hospitalapp.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Entity
@@ -12,9 +17,14 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 4, max = 50)
     private String nom;
     private String prenom;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
     private Date dateNaissance;
+    @DecimalMin("100")
     private int score;
     private boolean malade;
 }
--- src/main/java/net/fatima/hospitalapp/web/PatientController.java
+++ src/main/java/net/fatima/hospitalapp/web/PatientController.java
@@ -1,7 +1,9 @@
 package net.fatima.hospitalapp.web;
 
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import net.fatima.hospitalapp.entities.Patient;
 import net.fatima.hospitalapp.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +11,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -48,4 +53,34 @@ public class PatientController {
         return "redirect:/index?page="+page+"&keyword="+keyword;
 
     }
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients(){
+        return patientRepository.findAll();
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatients";
+    }
+    @PostMapping(path = "/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "") String keyword){
+        if(bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatients(Model model,Long id, String keyword,int page ){
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if(patient==null) throw new RuntimeException("Patient not found");
+        model.addAttribute("patient", patient);
+        model.addAttribute("keyword",keyword);
+        model.addAttribute("page",page);
+        return "editPatients";
+    }
+
 }
